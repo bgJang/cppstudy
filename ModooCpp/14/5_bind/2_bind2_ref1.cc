7ade9cb5b6d303aa40b592a5d8fc88ade1e4218c
bogyeongjang <cbg1015@naver.com>
2023-02-15 15:02:50 +0900

    bind 예제 #2 : bind에 객체를 넣었더니 복사가 실행되었음.

---
+++ ModooCpp/14/5_bind/2_bind2_ref1.cc
@@ -0,0 +1,40 @@
+// 씹어먹는 C ++ - <14. 함수를 객체로! (C++ std::function, std::mem_fn, std::bind)>
+// std::bind
+// https://modoocode.com/254#page-heading-4
+// 레퍼런스를 인자로 받는 함수
+
+#include <functional>
+#include <iostream>
+
+struct S {
+    int data;
+    S(int data) : data(data) {
+        std::cout << "일반 생성자 호출!" << std::endl;
+    }
+
+    S(const S& s) {
+        std::cout << "복사 생성자 호출!" << std::endl;
+        data = s.data;
+    }
+
+    S(S&& s) {
+        std::cout << "이동 생성자 호출!" << std::endl;
+        data = s.data;
+    }
+};
+
+void do_something(S& s1, const S& s2) {
+    s1.data = s2.data + 3;
+}
+
+int main() {
+    S s1(1), s2(2);
+
+    std::cout << "Before : " << s1.data << std::endl;
+
+    // s1 이 그대로 전달된 것이 아니라 s1 의 복사본이 전달됨!
+    auto do_something_with_s1 = std::bind(do_something, s1, std::placeholders::_1);
+    do_something_with_s1(s2);
+
+    std::cout << "After :: " << s1.data << std::endl;
+}
